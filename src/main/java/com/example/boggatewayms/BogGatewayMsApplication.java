a4a90f171476ec1bdb4753f8663d0f297209a068
Daniil Kovalyov <kovalyovd21@gmail.com>
2021-11-24 16:16:27 +0300

    Initial commit

---
+++ src/main/java/com/example/boggatewayms/BogGatewayMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.boggatewayms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogGatewayMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BogGatewayMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boggatewayms/BogGatewayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boggatewayms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogGatewayMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
