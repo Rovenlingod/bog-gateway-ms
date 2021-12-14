74f9cc291e9927649a2f1f0697965de1766c9bda
Daniil Kovalyov <kovalyovd21@gmail.com>
2021-12-14 18:40:08 +0300

    - feign path changed

--- src/main/java/com/example/boggatewayms/feign/UserServiceFeign.java
+++ src/main/java/com/example/boggatewayms/feign/UserServiceFeign.java
@@ -4,10 +4,11 @@ import com.example.boggatewayms.feign.feignDtos.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-service")
 public interface UserServiceFeign {
 
-    @GetMapping("/api/user/{login}")
-    UserDTO getUserByLogin(@PathVariable(name = "login") String login);
+    @GetMapping("/api/user")
+    UserDTO getUserByLogin(@RequestParam(name = "login") String login);
 }
